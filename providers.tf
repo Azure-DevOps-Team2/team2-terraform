bf5c274981942b30004c30ca059697382b97a516
Sharan <sharan.nagulapalli@twg.co.nz>
2021-03-19 13:26:16 +1300

    update provider

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
+      #source = "hashicorp/azurerm"
+      #version = ">= 2.26"
       skip_provider_registration = true
     }
   }
