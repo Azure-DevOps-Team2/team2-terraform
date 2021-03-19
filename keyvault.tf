06ce49f62871ea9dac6cd7e0798e4882476e41b0
Sharan <sharan.nagulapalli@twg.co.nz>
2021-03-19 13:09:49 +1300

    fix name

--- keyvault.tf
+++ keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "azure_vault" {
-  name                        = "AnimalName_Dev"
+  name                        = "AnimalNameDev"
   location                    = data.azurerm_resource_group.azuredevops.location
   resource_group_name         = data.azurerm_resource_group.azuredevops.name
   enabled_for_disk_encryption = true
